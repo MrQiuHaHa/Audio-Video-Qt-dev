09d94a754ef67f3bedc30c3eb25b172ed9993f73
davis <2574282239@qq.com>
2022-11-05 15:31:11 +0800

    show yuv image

---
+++ 21_sdl_show_yuv/mainwindow.cpp
@@ -0,0 +1,23 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include "playthread.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+
+void MainWindow::on_pushButton_clicked()
+{
+    PlayThread *thread = new PlayThread(this);
+    thread->start();
+}
+
